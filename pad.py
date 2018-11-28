2ff5452afa69e211dcf3998350cf2658fe04abce
logeirs <logeirs@hotmail.co.uk>
2018-11-28 13:03:22 +1100

    First commit

--- pad.py
+++ pad.py
@@ -2,25 +2,25 @@ import urllib
 
 
 BLOCK=8
-s="XiFnKGRM67n53Tf%2BZ3yyQa2iR%2FGM%2FStB"
-s = urllib.unquote(s).decode('utf8') 
+cipher="XiFnKGRM67n53Tf%2BZ3yyQa2iR%2FGM%2FStB"
+cipher = urllib.unquote(cipher).decode('utf8') 
 
-print s
+print cipher
 
-sd=s.decode('base64').encode('hex')
+cipher_hex=cipher.decode('base64').encode('hex')
 
-p1 = sd[:-BLOCK-2]
-p2 = sd[-BLOCK:]
+p1 = cipher_hex[:-BLOCK-2]
+p2 = cipher_hex[-BLOCK:]
 a=[]
 
 
 
-print sd
+print cipher_hex
 print p1, '', p2, '\n'
 
 with open('c.txt', 'wb') as f:
     for i in xrange(0,256):
-        newc = (p1+chr(i).encode('hex') + p2).decode('hex').encode('base64').replace('\n','')
-        f.write(urllib.quote_plus(newc)+'\n')
+        new_cipher = (p1+chr(i).encode('hex') + p2).decode('hex').encode('base64').replace('\n','')
+        f.write(urllib.quote_plus(new_cipher)+'\n')
         
 
